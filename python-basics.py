1c373f085ce6820b52eb2621d7a1331d5955bd68
Gowtham <48799978+Gowtham1392@users.noreply.github.com>
2023-06-28 22:14:44 +0900

    Update python-basics.py

--- python-basics.py
+++ python-basics.py
@@ -1,1 +1,12 @@
+from datetime import datetime
+class FirstClass:
+    def first_method(self):
+        name = input("Enter the name:")
+        year_of_birth = int(input("Enter year of birth:"))
+        current_year = datetime.now().year
+        age = current_year - year_of_birth
+        print(f"Age is {age}")
 
+
+firstClass = FirstClass()
+firstClass.first_method()
