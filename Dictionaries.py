c88dafde543002cb15b993690fdfae24845408db
gowtham <gowtham.1392@gmail.com>
2023-07-10 13:16:42 +0900

    After Dictionaries

---
+++ Dictionaries.py
@@ -0,0 +1,17 @@
+customer = {
+    "name":"XYZ",
+    "address":"Tokyo, Japan",
+    "pin":"12345",
+    "country":"Japan"
+}
+
+print(customer["address"])
+
+print(customer.get("name"))
+
+print(len(customer))
+
+print(type(customer))
+
+thisdict = dict(name = "John", age = 36, country = "Norway")
+print(thisdict)
